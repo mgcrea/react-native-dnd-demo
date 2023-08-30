a872c63b086cd6b60d21faa7ca8e10239675efbe
Olivier Louvignes <olivier@mgcrea.io>
2023-08-30 16:56:29 +0200

    chore(metro): add commented resolverMainFields config

--- metro.config.js
+++ metro.config.js
@@ -7,6 +7,11 @@ const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resolver: {
+    // unstable_enableSymlinks: true,
+    // resolverMainFields: ['react-native', 'browser', 'alt', 'main'],
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
