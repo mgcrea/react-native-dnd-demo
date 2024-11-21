497f815e06c631fe6dfb0dd4f8eba52d6ff8b361
Olivier Louvignes <olivier@mgcrea.io>
2024-11-21 18:22:01 +0100

    chore(rn): update to latest minor

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,6 @@ export const App: FunctionComponent = () => {
       />
       <GestureHandlerRootView style={{ flex: 1 }}>
         <BasicExample />
-        {/* <FlatListExample /> */}
       </GestureHandlerRootView>
     </SafeAreaView>
   );
